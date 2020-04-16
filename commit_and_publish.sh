edf7cb0b4d6ea4a1eb2b88274bf7efcc3b13baa5
Serge Kroes <serge@kroescontrol.nl>
2020-04-16 13:23:10 +0200

    quickcommits

--- commit_and_publish.sh
+++ commit_and_publish.sh
@@ -18,44 +18,44 @@ rm -rf .git/worktrees/public/
 git add --all && git commit -m "quickcommits" || git status -s
 
 sleep 1
-echo -n "Pulling latest ... "
+echo -n "[LIVE] Pulling latest ... "
 git pull -q --rebase
 
 git status
 
-echo -n "Pushing to github ... "
+echo -n "[LIVE] Pushing to github ... "
 git push -q origin master && echo "$(tput setaf 2)Everything up-to-date$(tput sgr0)" || echo echo "$(tput setaf 1) Failed!$(tput sgr0)"
 
 ###
 ### push to gh-pages showcase
 ###
-set -x
+
 git clone git@github.com:kroescontrol/showcase.kroescontrol.nl.git --branch gh-pages --single-branch public
 hugo --baseURL https://showcase.kroescontrol.nl
 cd public
 git add --all
-git commit -m "Publishing to gh-pages"
+git commit -m "[SHOWCASE] Publishing to gh-pages"
 git push -q origin gh-pages && echo "$(tput setaf 2)Everything up-to-date$(tput sgr0)" || echo echo "$(tput setaf 1) Failed!$(tput sgr0)"
 cd ..
 rm -Rf public
 
-echo "FINISHED showcase deploy"
+echo "[SHOWCASE] Finished publishing to gh-pages"
 exit 99
 
 ###
 ### push gh-pages LIVE WWW
 ###
 
-echo "Checking out gh-pages branch into public"
+echo "[LIVE] Checking out gh-pages branch into public"
 git worktree add -B gh-pages public origin/gh-pages
 
-echo "Generating site"
+echo "[LIVE] Generating site"
 hugo
 
-echo "Updating gh-pages branch"
+echo "[LIVE] Updating gh-pages branch"
 cd public 
 git add --all
-git commit -m "Publishing to gh-pages"
+git commit -m "[LIVE] Publishing to gh-pages"
 
-echo -n "Pushing to github gh-pages ... "
+echo -n "[LIVE] Pushing to github gh-pages ... "
 git push -q origin gh-pages && echo "$(tput setaf 2)Everything up-to-date$(tput sgr0)" || echo echo "$(tput setaf 1) Failed!$(tput sgr0)"
